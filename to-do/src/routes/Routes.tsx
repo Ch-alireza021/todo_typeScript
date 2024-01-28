a4244e81e7ee43dc91c540529c65176940aef178
Alireza Chizari <ch.alireza021@gmail.com>
2024-01-28 15:05:03 +0330

    feat:router

--- to-do/src/routes/Routes.tsx
+++ to-do/src/routes/Routes.tsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
+import Home from "../page/Home";
+import { PATH } from "../config/router/routerConfig";
+import AddTodo from "../page/AddTodo";
 
 const router=createBrowserRouter([{
     path:"/",
-    element:<Layout/>
+    element:<Layout/>,
+    children:[
+        {index:true,element:<Home/>},
+        {path:PATH.TODO,element:<AddTodo/>}
+    ]
 }])
 
 export default router;
