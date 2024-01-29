7dbdea8cac4e9eccfe4bed349df2fbe906350f84
Alireza Chizari <ch.alireza021@gmail.com>
2024-01-29 12:49:47 +0330

    style:creat useful style

---
+++ to-do/src/styles/CreatStyle.tsx
@@ -0,0 +1,12 @@
+import { Box, IconButton, styled } from "@mui/material";
+
+export const CustomIconButton=styled(IconButton)`
+    width: 20px;
+    height: 20px;
+`
+
+export const Flexrow=styled(Box)`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
