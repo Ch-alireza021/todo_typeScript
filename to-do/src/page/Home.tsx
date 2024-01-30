57a193f01d886400a24ee36fe3f62a2e64ae39cc
Alireza Chizari <ch.alireza021@gmail.com>
2024-01-30 15:29:26 +0330

    feat:synce to db-json

--- to-do/src/page/Home.tsx
+++ to-do/src/page/Home.tsx
@@ -1,9 +1,32 @@
-import Card from "../components/widget/card/Card"
+import { useQuery } from "@tanstack/react-query";
+import Card from "../components/widget/card/Card";
+import api from "../api/api";
+import { FlexColumn } from "../styles/creatStyle";
 
-const Home = () => {
-  return (
-    <Card/>
-  )
+export interface TodoType {
+  id: string;
+  title: string;
+  description: string;
+  duedate: string;
+  isDone:boolean
 }
 
-export default Home
+const Home = () => {
+  const { data: todos } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const data = await api.get("todo");
+      return data.data;
+    },
+  });
+  console.log(todos);
+  return (
+    <FlexColumn>
+      {todos?.map((todo:TodoType) => (
+        <Card data={todo} key={todo.id} />
+      ))}
+    </FlexColumn>
+  );
+};
+
+export default Home;
