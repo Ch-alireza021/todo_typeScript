5301b51728905df98deb53c290290e3e3803dc5c
Alireza Chizari <ch.alireza021@gmail.com>
2024-01-30 15:29:59 +0330

    feat:show content

--- to-do/src/components/widget/card/Card.tsx
+++ to-do/src/components/widget/card/Card.tsx
@@ -1,26 +1,34 @@
-import React  from 'react'
+import React, { FC } from "react";
 
-import {  Box, styled } from "@mui/material"
-import { ThemeContext } from '@emotion/react'
-import CardHeader from './CardHeader'
-import CardContent from './CardContent'
-const CustomBox=styled(Box)`
-box-sizing: border-box;
-width: 100%;
-height: 100px;
-border-radius: 10px;
-padding: 5px 10px;
-`
+import { Box, styled } from "@mui/material";
+import { ThemeContext } from "@emotion/react";
+import CardHeader from "./CardHeader";
+import CardContent from "./CardContent";
+import { TodoType } from "../../../page/Home";
 
-const Card = () => {
-    const mode=React.useContext(ThemeContext).palette.mode
-    console.log(mode);
-  return (
-    <CustomBox sx={{background:mode==="dark"? "#197f754f":"#0300024f"}}>
-        <CardHeader/>
-        <CardContent/>
-    </CustomBox>
-  )
+const CustomBox = styled(Box)`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100px;
+  border-radius: 10px;
+  padding: 5px 10px;
+`;
+
+interface CardProps {
+  data: TodoType;
 }
 
-export default Card
+const Card: FC<CardProps> = ({ data }) => {
+  const mode = React.useContext(ThemeContext).palette.mode;
+
+  return (
+    <CustomBox sx={{ background: mode === "dark" ? "#197f754f" : "#0300024f" }}>
+      <CardHeader />
+      <CardContent
+        data={{ description: data?.description, isDone: data.isDone }}
+      />
+    </CustomBox>
+  );
+};
+
+export default Card;
