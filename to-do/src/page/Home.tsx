0939346c2d0f1766a06f1c33d7f3d3660d353bef
Alireza Chizari <ch.alireza021@gmail.com>
2024-02-02 14:24:39 +0330

    improve sorting data

--- to-do/src/page/Home.tsx
+++ to-do/src/page/Home.tsx
@@ -3,13 +3,14 @@ import Card from "../components/widget/card/Card";
 import api from "../api/api";
 import { FlexColumn } from "../styles/creatStyle";
 import Paginate from "../components/pagination/paginate";
-import { useState } from "react";
+import {  useState } from "react";
+import setIsDone from "../feature/date/setIsDone";
 
 export interface TodoType {
   id: string;
   title: string;
   description: string;
-  duedate: string;
+  dueDate: string;
   isDone: boolean;
   edited: string | undefined;
 }
@@ -30,16 +31,45 @@ const Home = () => {
   };
   // ---------------------------------------------
   // Separate data based on each page
+  const sortingData = () => {
+    let expireDate: TodoType[] = [];
+    let unExpireDate: TodoType[] = [];
+    todos?.forEach((el: TodoType) => {
+      if (new Date(el.dueDate).getTime() < Date.now() || el.isDone === true) {
+        if (el.isDone !== true) {
+          // If the time has passed, its status changes to the state of completion
+          setIsDone(el.id, false);
+          el.isDone = true;
+        }
+        expireDate.push(el);
+      } else {
+        unExpireDate.push(el);
+      }
+    });
+
+    const data = [
+      ...unExpireDate?.sort(
+        (a: TodoType, b: TodoType) =>
+          new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime()
+      ),
+      ...expireDate?.sort(
+        (a: TodoType, b: TodoType) =>
+          new Date(b.dueDate).getTime() - new Date(a.dueDate).getTime()
+      ),
+    ];
+    return data;
+  };
+//  -------------------------------------
+//            Displayed todo data
+  const data = sortingData();
   const dataOfpage = (todos: TodoType[], page: number) => {
-    const sortData=todos?.sort((a,b)=>+(a.duedate) - +(b.duedate))
-    console.log(sortData);
     const data = todos?.slice((page - 1) * 5, page * 5);
     return data;
   };
 
   return (
     <FlexColumn>
-      {dataOfpage(todos, page)?.map((todo: TodoType) => (
+      {dataOfpage(data, page)?.map((todo: TodoType) => (
         <Card data={todo} key={todo.id} />
       ))}
       <Paginate length={todos?.length} page={{ page, handlePage }} />
